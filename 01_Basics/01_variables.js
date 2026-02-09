8cffdf3b938df211756c0ffc9b96db03c67bba33
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-02-09 14:22:58 +0000

    var,let,,const variable

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = "1589"
+let accountEmail = "ritik@gmail.com"  // accountEmail this is a variable
+var accountPassword = "9823"
+accountCity = "Luckkhnow"
+
+// acountId = 2 not allowed not change values
+accountEmail = "kumar@gmial.com"
+accountPassword = "0000"
+accountCity = "Noida"
+let accountState; // 
+/*
+prefer not to use var 
+because of issues in block scope and function scope
+ */
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
