d3cacd35246641df5ae47dc58bc6717c81fbeaaa
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-02-10 17:43:40 +0000

    Data Types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"user strict"; // treat all js code as newer version
+
+// alert (3+3) we are using node js , not browser, alert statement hai
+console.log(3
+    +3)// code raadability should be high
+console.log("Ritik")
+
+let name = "ritik"
+let age = 20
+let isLoggrdIn = false
+// let isLoggrdin = true
+let state; // this is undefined value
+let temperature = null // null means empty 
+let computer = undefined;
+
+// numeric => 2 ki power 53
+// bigint use hota hai big data kai liye as like : flipkart,treding mai ,stock markert mai yaha par number big consume karta hai
+// string => ""
+// boolean => true,or false
+// null, ak data type hai or => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof "ritik"); // output string
+console.log(typeof 20); // output number
+console.log(typeof null); // object ye sab type hai
+console.log(typeof undefined);
+
+
+
+
+
+
