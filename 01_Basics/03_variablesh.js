d5492197c8b5dea0928935c8a30d8d7028ec0870
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-02-16 09:24:32 +0000

    Comparison Data Type

---
+++ 01_Basics/03_variablesh.js
@@ -0,0 +1,27 @@
+let score = "33hsa"
+// let score = "abc"; output is NaN 
+
+// let score = null ; output is 0
+// let score = undefined; output is NaN aata hai
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let sco = "45"
+console.log(typeof sco); // 
+console.log(typeof (sco));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(true);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+
+
